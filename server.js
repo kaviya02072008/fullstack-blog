58ef4913cfc6ff2e52e05ee120a0c7d07cd829b1
kaviya02072008 <kaviyasivaselvi02@gmail.com>
2026-07-27 21:56:35 +0530

    Day 2 - Created Home and Add Blog pages with HTML and CSS

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/",(req,res) => {
-    res.send("Hello World!");
-});
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
